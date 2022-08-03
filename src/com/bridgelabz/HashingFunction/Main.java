cd9afa50739b625b2796244431283a3598fa6ef2
Resh@308 <reshsingh1995@gmail.com>
2022-08-03 11:09:57 +0530

    Added: Hasg Function  Data Structure Problems

---
+++ src/com/bridgelabz/HashingFunction/Main.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.HashingFunction;
+
+public class Main {
+    public static void main(String[] args) {
+        HashFunc func = new HashFunc();
+        func.hashTable();
+        func.showTable();
+        func.fileWrite();
+
+    }
+
+}
