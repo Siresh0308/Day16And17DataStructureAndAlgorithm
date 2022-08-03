42fe2a21b00fd2d96a4344de27126979adc2c1ae
Resh@308 <reshsingh1995@gmail.com>
2022-08-03 11:30:28 +0530

    Added: Prime number Data Structure Problems

---
+++ src/com/bridgelabz/PrimeNumber2D/Main.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.PrimeNumber2D;
+
+public class Main {
+    public static void main(String[] args) {
+        PrimeTwoDim primeNo = new PrimeTwoDim();
+        primeNo.primeNo();
+        primeNo.show();
+
+    }
+
+
+}
