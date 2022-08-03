f26ff67b88a7a48d3eaf5eabe626f2ad87e7f8b2
Resh@308 <reshsingh1995@gmail.com>
2022-08-03 11:32:53 +0530

    Added: Unordered list  Data Structure Problems

---
+++ src/com/bridgelabz/Unorderedlist/Main.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.Unorderedlist;
+
+import java.util.Scanner;
+
+public class Main {
+    public static  void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the text to search :");
+        String input = scan.nextLine();
+        UnorderedList<String> func = new UnorderedList<String>(input);
+        System.out.println(func.search());
+    }
+
+}
