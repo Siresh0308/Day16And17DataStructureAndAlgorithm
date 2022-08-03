031ce77e6f27c77b3a3539cd5f6e2b2f6d145fdd
Resh@308 <reshsingh1995@gmail.com>
2022-08-03 11:28:31 +0530

    Added: Palindrom Anagram stack Data Structure Problems

---
+++ src/com/bridgelabz/PrimeAnagramStack/Node.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.PrimeAnagramStack;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data=data;
+        this.next=null;
+        Stack.size++;
+    }
+}
